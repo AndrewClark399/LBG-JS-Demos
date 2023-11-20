200db51c29a19ebc0da6145d55135850257a878b
AndrewClark399 <andy396clark@hotmail.co.uk>
2023-11-20 11:58:50 +0000

    added new files

---
+++ 15_JavaScript/index.js
@@ -0,0 +1,20 @@
+const amount = 300;
+console.log(amount);
+
+let fruit = "apple";
+console.log(fruit);
+
+let red = true;
+console.log(red);
+
+ red = false;
+ console.log(red);
+
+ amount = "three hundred";
+ console.log(amount);
+
+ let demo;
+ console.log(demo);
+
+ demo = null
+ console.log(demo);
